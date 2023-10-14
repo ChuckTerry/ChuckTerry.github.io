21faa7ceea0953607f6dd6995b3badbac7a7c256
Chuck Terry <Chuck.Terry@Outlook.com>
2023-10-14 03:46:55 +0000

    Create interim Variant Class

---
+++ dice/js/DieVariant.js
@@ -0,0 +1,55 @@
+import { InstanceController } from './InstanceController';
+
+/***********************************************************************
+ * Represents a die variant.
+ ***********************************************************************/
+export class DieVariant {
+
+  /**
+   * @param {InstanceController} instanceController 
+   */
+  constructor(instanceController) {
+
+    /** @type {InstanceController} */
+    this.instanceController = instanceController;
+
+    /** @type {function (number): string} */
+    this.contour = (x) => `rgb(${x - 150}, ${x - 16}, ${x + 16})`;
+
+    /** @type {string} */
+    this.valueDotColor = '#DDEEFF';
+
+    /** @type {string} */
+    this.bodyFill = 'rgb(46, 184, 208)';
+
+    const urlParameters = new URLSearchParams(window.location.search);
+    if (urlParameters.has('style')) {
+      const variant = urlParameters.get('style').toUpperCase();
+      if (variant === 'RED') {
+        this.setRedStyle();
+      } else if (variant === 'BLACK') {
+        this.setRedStyle();
+      }
+    }
+
+  }
+
+  /**
+   * Set the die variant to black.
+   */
+  setBlackStyle() {
+    this.contour = (x) => `rgb(${x}, ${x - 3}, ${x - 7})`;
+    this.valueDotColor = '#000000';
+    this.bodyFill = '#FFFFFF';
+  }
+
+  /**
+   * Set the die variant to red.
+   */
+  setRedStyle() {
+    this.contour = (x) => `rgb(${x}, ${x - 166}, ${x - 149})`;
+    this.valueDotColor = '#FFFEFE';
+    this.bodyFill = 'rgb(208, 45, 47)';
+  }
+
+}
--- dice/js/index.js
+++ dice/js/index.js
@@ -1,4 +1,5 @@
 import { InstanceController } from './InstanceController.js';
+import { DieVariant } from './DieVariant.js';
 import './globals.js';
 
 /**
@@ -7,27 +8,9 @@ import './globals.js';
 function init() {
   if (document.readyState !== 'complete') return window.addEventListener('load', init);
 
-  const urlParameters = new URLSearchParams(window.location.search);
-  if (urlParameters.has('style')) {
-    const variant = urlParameters.get('style').toUpperCase();
-    if (variant === 'RED') {
-      dieVariantObject.contour = (x) => `rgb(${x}, ${x - 166}, ${x - 149})`;
-      dieVariantObject.valueDotColor = '#FFFEFE';
-      dieVariantObject.bodyFill = 'rgb(208, 45, 47)';
-    } else if (variant === 'BLACK') {
-      dieVariantObject.contour = (x) => `rgb(${x}, ${x - 3}, ${x - 7})`;
-      dieVariantObject.valueDotColor = '#000000';
-      dieVariantObject.bodyFill = '#FFFFFF';
-    }
-  }
-
-  if (Object.keys(dieVariantObject).length === 0) {
-    dieVariantObject.contour = (x) => `rgb(${x - 150}, ${x - 16}, ${x + 16})`;
-    dieVariantObject.valueDotColor = '#DDEEFF';
-    dieVariantObject.bodyFill = 'rgb(46, 184, 208)';
-  }
-  
   globalThis.instanceController = new InstanceController();
+  globalThis.dieVariantObject = new DieVariant(globalThis.instanceController);
+
 }
 
 init();
