c112720d44d0e90f6faf4dca16a6bcfff8a4d847
Chuck Terry <Chuck.Terry@Outlook.com>
2023-10-14 03:20:05 +0000

    Docs & cleanup

--- dice/js/InputManager.js
+++ dice/js/InputManager.js
@@ -7,7 +7,6 @@ import './globals.js';
 export class InputManager {
 
   /**
-   * 
    * @param {InstanceController} instanceController - The display object.
    * @param {boolean} [autoRegister] - Should we automatically register event listeners?
    */
@@ -19,7 +18,73 @@ export class InputManager {
     /** @type {Display} */
     this.display = instanceController.display;
 
-    if (autoRegister) this.registerListeners();
+    if (autoRegister) {
+      this.registerListeners();
+    }
+  }
+
+  /**
+   * Redirector for mouseDown Event
+   * @param {MouseEvent} event 
+   */
+  onMousedown(event) {
+    return this.#selectStart(event);
+  }
+
+  /**
+   * Redirector for mouseMove Event
+   * @param {MouseEvent} event 
+   */
+  onMousemove(event) {
+    return this.#selectMove(event);
+  }
+
+  /**
+   * Redirector for mouseUp Event
+   * @param {MouseEvent} event 
+   */
+  onMouseup(event) {
+    return this.#selectEnd(event);
+  }
+
+  /**
+   * Redirector for touchEnd Event
+   * @param {TouchEvent} event 
+   */
+  onTouchend(event) {
+    return this.#selectEnd(event);
+  }
+
+  /**
+   * Redirector for touchMove Event
+   * @param {TouchEvent} event 
+   */
+  onTouchmove(event) {
+    return this.#selectMove(event);
+  }
+
+  /**
+   * Redirector for touchStart Event
+   * @param {TouchEvent} event 
+   */
+  onTouchstart(event) {
+    return this.#selectStart(event);
+  }
+
+  /**
+   * Registers event listeners.
+   */
+  registerListeners() {
+    const eventNames = ['touchstart', 'mousedown', 'touchmove', 'mousemove', 'touchend', 'mouseup'];
+    const eventCount = eventNames.length;
+    for (let index = 0; index < eventCount; index++) {
+      const eventName = eventNames[index];
+      const onName = `on${eventName.charAt(0).toUpperCase()}${eventName.substring(1)}`;
+      window.addEventListener(eventName, (event) => {
+        event.preventDefault();
+        return this[onName](event) ?? false;
+      });
+    }
   }
 
   /**
@@ -38,7 +103,9 @@ export class InputManager {
     this.nsx = event.clientX ?? event.touches[0].clientX;
     this.nsy = event.clientY ?? event.touches[0].clientY;
     this.srs = new Vertex(this.nsx - (this.display.width >> 1), this.nsy - (this.display.height >> 1), 0);
-    if (!this.isDragging) return;
+    if (!this.isDragging) {
+      return;
+    }
     const divisor = Display.bounds >> 4;
     this.instanceController.dice.addRotation(Math.atan((this.nsy - this.osy) / divisor), -Math.atan((this.nsx - this.osx) / divisor));
     this.osx = this.nsx;
@@ -50,33 +117,13 @@ export class InputManager {
    * @param {MouseEvent | TouchEvent} event 
    */
   #selectStart(event) {
-    if (event.target.nodeName !== 'CANVAS') return true;
+    if (event.target.nodeName !== 'CANVAS') {
+      return true;
+    }
     this.isx = this.osx = event.clientX ?? event.touches[0].clientX;
     this.isy = this.osy = event.clientY ?? event.touches[0].clientY;
     this.instanceController.dice.stopRotation();
     this.isDragging = true;
   }
 
-  onMousedown(event) { return this.#selectStart(event); }
-  onMousemove(event) { return this.#selectMove(event); }
-  onMouseup(event) { return this.#selectEnd(event); }
-  onTouchend(event) { return this.#selectEnd(event); }
-  onTouchmove(event) { return this.#selectMove(event); }
-  onTouchstart(event) { return this.#selectStart(event); }
-
-  /**
-   * Registers event listeners.
-   */
-  registerListeners() {
-    const eventNames = ['touchstart', 'mousedown', 'touchmove', 'mousemove', 'touchend', 'mouseup'];
-    const eventCount = eventNames.length;
-    for (let index = 0; index < eventCount; index++) {
-      const eventName = eventNames[index];
-      const onName = `on${eventName.charAt(0).toUpperCase()}${eventName.substring(1)}`;
-      window.addEventListener(eventName, (event) => {
-        event.preventDefault();
-        return this[onName](event) ?? false;
-      });
-    }
-  }
 }
