6c8a3887e53f6ecfe0a9e633202b868d4c8e878c
Chuck Terry <Chuck.Terry@Outlook.com>
2024-12-14 18:08:35 -0500

    Create script.js

---
+++ graphic-design/threshold/script.js
@@ -0,0 +1,12 @@
+++import { CurrentImage } from "./scripts/CurrentImage.js";
+import { ConfigForm } from "./scripts/ConfigForm.js";
+
+function init() {
+    new ConfigForm(new CurrentImage());
+}
+
+if (window.readyState === 'complete') {
+    init();
+} else {
+    window.addEventListener('load', init);
+}
